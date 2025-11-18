1c66f3dffa349c8cb6c365fe3b2a2f47ab623839
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-11-18 12:40:25 -0700

    fix: bump bandit

--- setup.py
+++ setup.py
@@ -2,48 +2,48 @@ import re
 
 import setuptools
 
-with open('README.md', 'r') as readme_file:
+with open("README.md", "r") as readme_file:
     long_description = readme_file.read()
 
 # Inspiration: https://stackoverflow.com/a/7071358/6064135
-with open('muffinscript/_version.py', 'r') as version_file:
+with open("muffinscript/_version.py", "r") as version_file:
     version_groups = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file.read(), re.M)
     if version_groups:
         version = version_groups.group(1)
     else:
-        raise RuntimeError('Unable to find version string!')
+        raise RuntimeError("Unable to find version string!")
 
 REQUIREMENTS = []
 
 DEV_REQUIREMENTS = [
-    'bandit == 1.8.*',
-    'black == 25.*',
-    'flake8 == 7.*',
-    'isort == 7.*',
-    'mypy == 1.18.*',
-    'pyinstaller == 6.*',
-    'pytest == 8.*',
-    'pytest-cov == 7.*',
+    "bandit == 1.9.*",
+    "black == 25.*",
+    "flake8 == 7.*",
+    "isort == 7.*",
+    "mypy == 1.18.*",
+    "pyinstaller == 6.*",
+    "pytest == 8.*",
+    "pytest-cov == 7.*",
 ]
 
 setuptools.setup(
-    name='muffinscript',
+    name="muffinscript",
     version=version,
-    description='Delectable little programming language.',
+    description="Delectable little programming language.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/justintime50/muffinscript',
-    author='justintime50',
-    license='MIT',
+    url="http://github.com/justintime50/muffinscript",
+    author="justintime50",
+    license="MIT",
     packages=setuptools.find_packages(
         exclude=[
-            'examples',
-            'test',
+            "examples",
+            "test",
         ]
     ),
     package_data={
-        'muffinscript': [
-            'py.typed',
+        "muffinscript": [
+            "py.typed",
         ]
     },
     classifiers=[
@@ -53,12 +53,12 @@ setuptools.setup(
     ],
     install_requires=REQUIREMENTS,
     extras_require={
-        'dev': DEV_REQUIREMENTS,
+        "dev": DEV_REQUIREMENTS,
     },
     entry_points={
-        'console_scripts': [
-            'muffinscript=muffinscript.muffin:main',
+        "console_scripts": [
+            "muffinscript=muffinscript.muffin:main",
         ]
     },
-    python_requires='==3.14.*',
+    python_requires="==3.14.*",
 )
